56323bc856977e5b12a4a6b27d343050a4ad8444
Brendan Coates <bcoates@ucsb.edu>
2016-08-03 15:35:00 -0700

    works 1.0

--- qct-parse2.py
+++ qct-parse2.py
@@ -3,12 +3,16 @@
 from lxml import etree
 xml = "S:/avlab/qct-parse/cpb-aacip-507-9p2w37mc70.mxf.qctools.xml"
 path = []
-framelist = []
+overdict = {}
 for event, elem in etree.iterparse(xml, events=('end',), tag='frame'):
 	if elem.attrib['media_type'] == "video":
-		print elem.attrib
-		foo = raw_input("eh")
+		for t in list(elem):
+			if t.attrib['key'] == 'lavfi.signalstats.YMAX':
+				if int(t.attrib['value']) > 239:
+					overdict[elem.attrib['best_effort_timestamp_time']] = t.attrib['value']
+					#foo = raw_input("eh")
 	elem.clear()
+print len(overdict)
 #if event == 'start':
 #		path.append(elem.tag)
 #		if elem.tag == 'tag':
