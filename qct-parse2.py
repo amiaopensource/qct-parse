3afed8cb9d647b2d3197f86a5b972d6cdaaf9225
Brendan Coates <bcoates@ucsb.edu>
2016-08-03 14:51:30 -0700

    tooling around, adds lxml

--- qct-parse2.py
+++ qct-parse2.py
@@ -1,8 +1,18 @@
 #qct-parse2
-import xml.etree.ElementTree as etree
-
+#import xml.etree.ElementTree as etree
+from lxml import etree
 xml = "S:/avlab/qct-parse/cpb-aacip-507-9p2w37mc70.mxf.qctools.xml"
-
-for event, elem in etree.iterparse(xml, events=('start','end','start-ns','end-ns')):
-	print event, elem
-	foo = raw_input("eh")
+path = []
+framelist = []
+for event, elem in etree.iterparse(xml, events=('end',), tag='frame'):
+	if elem.attrib['media_type'] == "video":
+		print elem.attrib
+		foo = raw_input("eh")
+	elem.clear()
+#if event == 'start':
+#		path.append(elem.tag)
+#		if elem.tag == 'tag':
+#			if elem.attrib['key'] == 'lavfi.signalstats.YMAX':
+#				print path
+#				#if elem.attrib['value'] >= 233:
+#					#framelist.append
