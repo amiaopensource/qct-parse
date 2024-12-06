e6842da24f3d63db3fe8ea8d6af3cf4773eb119d
Henry Borchers <henryborchers@yahoo.com>
2024-12-06 12:39:20 -0600

    add test suite files for overcatch.py and makeqctoolsreport.py

---
+++ tests/test_makeqctoolsreport.py
@@ -0,0 +1,5 @@
+# Note: This is currently an empy test suite and contains no actual tests.
+# Until tests are written, it still serves the purpose to see if the module can
+# be imported.
+
+from qct_parse import makeqctoolsreport
